504e296dcda11dd23644773abfb3e69b2bf6d1b4
AmAr <amar.glwsh28@gmail.com>
2024-12-16 01:54:14 +0200

    forth commit

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 // setting game name
-let gameName = "Guess the word";
+let gameName = "Guess The Word";
 document.title = gameName;
 document.querySelector("h1").innerHTML = gameName;
 document.querySelector(
   "footer"
 ).innerHTML = `${gameName} game <br /> Created by <a href="https://www.linkedin.com/in/amar-ghalwash-80aa70277/" target="_blank">Ammar Ghalwash</a>
-  under the supervision of <a href="https://www.linkedin.com/in/osamaelzero/" target="_blank">El-Zero</a>
+  <br /> Under the supervision of <a href="https://www.linkedin.com/in/osamaelzero/" target="_blank">El-Zero</a>
 `;
 
 // setting game options
